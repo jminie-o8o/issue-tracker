9ee96a8656c86111cb8b1627ff195ca7f16f0b5e
Senglee Choi <tmdfl1027@gmail.com>
2022-06-29 13:58:36 +0900

    Fix: LabelListResponseDto 게터 추가
    라벨 목록보기 조회 안 되는 문제 해결.

--- BE/src/main/java/kr/codesquad/issuetraker/dto/LabelListResponseDto.java
+++ BE/src/main/java/kr/codesquad/issuetraker/dto/LabelListResponseDto.java
@@ -1,8 +1,11 @@
 package kr.codesquad.issuetraker.dto;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class LabelListResponseDto {
     private List<LabelListElementDto> labels;
 
