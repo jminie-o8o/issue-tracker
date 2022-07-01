6676c1e90671944b1eca745ce1b82f5610f218ac
Senglee Choi <tmdfl1027@gmail.com>
2022-07-01 23:01:12 +0900

    Chore: 불필요한 어노테이션 삭제
    JpaRepsitory에서 @Repository 어노테이션이 불필요하여 삭제

--- BE/src/main/java/kr/codesquad/issuetraker/domain/issue/IssueRepository.java
+++ BE/src/main/java/kr/codesquad/issuetraker/domain/issue/IssueRepository.java
@@ -2,11 +2,9 @@ package kr.codesquad.issuetraker.domain.issue;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface IssueRepository extends JpaRepository<Issue, Long>, IssueRepositoryCustom {
     @Query(value = "SELECT i FROM Issue i where i.isDeleted = FALSE")
     public List<Issue> findAllAvailableIssues();
