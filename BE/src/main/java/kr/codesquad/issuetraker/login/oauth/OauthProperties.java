0c890c384c6c94a9061caee0f301e32b2efe075a
Senglee Choi <tmdfl1027@gmail.com>
2022-07-02 03:34:20 +0900

    Refactor: OauthProperties 불필요한 어노테이션 삭제

--- BE/src/main/java/kr/codesquad/issuetraker/login/oauth/OauthProperties.java
+++ BE/src/main/java/kr/codesquad/issuetraker/login/oauth/OauthProperties.java
@@ -1,14 +1,10 @@
 package kr.codesquad.issuetraker.login.oauth;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class OauthProperties {
     private String clientId;
     private String authServerUrl;
