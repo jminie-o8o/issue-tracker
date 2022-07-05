3eb2ff4b67a3b2f06604787d4bdd0a3fd70c6a27
junseokseo <junseoks9306@gmail.com>
2022-07-05 11:05:52 +0900

    feat: OAuth 통신 후 accessToken 저장하기 기능 추가 (#48)

--- android/app/src/main/java/com/example/issue_tracker/ui/login/LoginViewModel.kt
+++ android/app/src/main/java/com/example/issue_tracker/ui/login/LoginViewModel.kt
@@ -1,26 +1,31 @@
 package com.example.issue_tracker.ui.login
 
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.issue_tracker.model.GitHubOAuthRequest
+import com.example.issue_tracker.datastore.CustomDataStore
+import com.example.issue_tracker.model.OAuthRequest
 import com.example.issue_tracker.network.CEHModel
 import com.example.issue_tracker.network.CoroutineException
 import com.example.issue_tracker.repository.LoginRepository
-import com.example.issue_tracker.ui.common.MainApplication
+import com.example.issue_tracker.ui.HomeActivity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val loginRepository: LoginRepository): ViewModel() {
-
-    private val _accessToken = MutableStateFlow<String?>(null)
-    val accessToken: StateFlow<String?> = _accessToken
+class LoginViewModel @Inject constructor(
+    private val loginRepository: LoginRepository,
+    private val dataStore: CustomDataStore,
+) : ViewModel() {
 
     private val _error = MutableStateFlow(CEHModel(null, ""))
     val error: SharedFlow<CEHModel> = _error.asSharedFlow()
@@ -29,14 +34,24 @@ class LoginViewModel @Inject constructor(private val loginRepository: LoginRepos
         _error.value = CoroutineException.checkThrowable(throwable)
     }
 
-    fun requestGitHubLogin(gitHubOAuthRequest: GitHubOAuthRequest) {
-        viewModelScope.launch(exceptionHandler) {
-            val response = loginRepository.requestGitHubLogin(gitHubOAuthRequest)
-            val accessToken =  response.accessToken.token
-            _accessToken.value = accessToken
+    fun requestGitHubLogin(context: Context, gitHubOAuthRequest: OAuthRequest) {
+        viewModelScope.launch {
+            val jwt =
+                withContext(Dispatchers.Default) {
+                    loginRepository.requestGitHubLogin(gitHubOAuthRequest)
+                }
+            dataStore.saveJwt(CustomDataStore.JWT_KEY, jwt.accessToken)
 
-            // SharedPreference 에 accessToken 저장
-            MainApplication.prefs.setString("accessToken", accessToken)
+            goToHomeActivity(context)
         }
+    }
+
+    private fun goToHomeActivity(context: Context) {
+        val intent = Intent(context, HomeActivity::class.java)
+        startActivity(context, intent, null)
+    }
+
+    companion object Companion {
+        const val INITIAL_JWT = ""
     }
 }
